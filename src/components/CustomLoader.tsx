e7d60c6ddfed6628eac0c47b527cada81bf77425
Ivan Korneychuk <ikorneyc@uwaterloo.ca>
2024-09-29 20:06:37 -0400

    Updated custom loader

--- src/components/CustomLoader.tsx
+++ src/components/CustomLoader.tsx
@@ -3,7 +3,7 @@ import { Loader2 } from "lucide-react";
 const CustomLoader = () => {
   return (
     <div className="flex items-center justify-center w-full h-screen">
-      <Loader2 className=" animate-spin size-10" />
+      <Loader2 className="text-orange-500 animate-spin size-10" />
     </div>
   );
 };
